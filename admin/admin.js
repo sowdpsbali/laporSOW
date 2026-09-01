73df724698687b845f7721fd656e03d1669d4af5
sowdpsbali1 <gedeariawan106@gmail.com>
2026-09-01 22:18:42 +0800

    NEW  code url

--- admin/admin.js
+++ admin/admin.js
@@ -8,7 +8,7 @@
 // =====================================================
 
 const API_URL =
-    "https://script.google.com/macros/s/AKfycbxV7gW_4aym9D9Mg-TZ43-zB4IkuYSyq809naBXNsiyzDLEr9rjnO79AHGc6F9ZNpIwAw/exec";
+    "https://script.google.com/macros/s/AKfycbyZAxvr-Ha8fYcA3sEb7Wrd_RVoeEDVtpp7tyzCIkRnHnTtCBkrmvgaOmTnVpZlsEZzhw/exec";
 
 
 // =====================================================
