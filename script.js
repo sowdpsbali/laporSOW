4b7629370ff5f81a04c4cd56f27743c2b926e1ea
sowdpsbali1 <gedeariawan106@gmail.com>
2026-09-03 21:35:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 ========================================= */
 
 const WEB_APP_URL =
-    "https://script.google.com/macros/s/AKfycbyZAxvr-Ha8fYcA3sEb7Wrd_RVoeEDVtpp7tyzCIkRnHnTtCBkrmvgaOmTnVpZlsEZzhw/exec";
+    "https://script.google.com/macros/s/AKfycbzb7Ur5bfCffsUjYb2_C1CmcMELCzNMD9c_8S4UFLmhouFftxgoMY4nnh_Op8lb7jzepA/exec";
 
 const NOMOR_ADMIN = "6281246443838";
 
